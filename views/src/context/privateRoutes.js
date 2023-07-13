5211b1ad47ff7de8a0e2875f4c2c1560d8173204
Aryan Rai <aryanx23@pop-os.localdomain>
2023-07-14 02:59:58 +0530

    reverting last change

--- views/src/context/privateRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Outlet, Navigate } from 'react-router-dom';
-export default function PrivateRoutes(){
-    let auth=localStorage.getItem("isAuthenticated");
-    return auth && auth !== null ? (
-        <Outlet />
-    ) : (
-        <Navigate to="/chatify-frontend/" replace />
-    );
-}
