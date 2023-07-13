8466327458f7e8e7779e59ed3939fe0f79139bd4
Aryan Rai <aryanx23@pop-os.localdomain>
2023-07-13 16:43:55 +0530

    changing cors options

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
     cors({
-        origin: process.env.ORIGIN_URL,
         credentials: true,
+        origin: true,
     })
 );
 
--- middlewares/socket.js
+++ middlewares/socket.js
@@ -5,7 +5,8 @@ module.exports = {
     init: (httpServer) => {
         io = require("socket.io")(httpServer, {
             cors: {
-                origin: process.env.ORIGIN_URL,
+                credentials: true,
+                origin: true
             },
         });
         return io;
