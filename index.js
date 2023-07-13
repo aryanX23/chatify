58e9e9a092a7292cdbf17d754d34707851eba49f
Aryan Rai <aryanx23@pop-os.localdomain>
2023-07-14 02:39:50 +0530

    hehe

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ app.use(
         origin: process.env.ORIGIN_URL,
     })
 );
+const allowCrossDomain = (req, res, next) => {
+    res.header(`Access-Control-Allow-Origin`, `https://aryan-rai.me/chatify-frontend/`);
+    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+    res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+    next();
+};
+app.use(allowCrossDomain);
 
 
 app.use('/api/users', userRouter);
