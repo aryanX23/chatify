94c9266ad248ec5bb1973fcd05af14204a0d091a
Aryan Rai <aryanx23@pop-os.localdomain>
2023-07-15 22:42:16 +0530

    adding changes

--- middlewares/socket.js
+++ middlewares/socket.js
@@ -5,8 +5,9 @@ module.exports = {
     init: (httpServer) => {
         io = require("socket.io")(httpServer, {
             cors: {
-                credentials: true,
-                origin: process.env.ORIGIN_URL
+            	origin: "*",
+                credentials: true
+                
             },
         });
         return io;
